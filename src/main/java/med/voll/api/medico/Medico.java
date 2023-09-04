942df2ad493c026556fbcafd472ced090ef7ffb2
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-09-04 18:21:50 -0300

    delete doctors

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -37,6 +37,8 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Medico(DadosCadastroMedico dados){
         this.nome = dados.nome();
         this.email = dados.email();
@@ -44,6 +46,7 @@ public class Medico {
         this.telefone = dados.telefone();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+        this.ativo = true;
     }
 
     public void atualizarInformacoes(DadosAtualizacaoMedico dados){
@@ -56,6 +59,10 @@ public class Medico {
         if(dados.endereco() != null){
             this.endereco.atualizarInformacoes(dados.endereco());
         }
+    }
+
+    public void excluir(){
+        this.ativo = false;
     }
 
 }
